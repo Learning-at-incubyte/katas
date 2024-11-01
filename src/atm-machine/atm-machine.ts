8e0b84767556de1688fd0c32263b9348cb0a9564
Vijay Makwana <vijay@incubyte.co>
2024-11-01 12:34:42 +0530

    ATM Machine withdrawal handle quantity

--- src/atm-machine/atm-machine.ts
+++ src/atm-machine/atm-machine.ts
@@ -1,55 +1,58 @@
 enum CurrencyType {
     BILL = 'bill',
-    BILLS = 'bills',
     COIN = 'coin',
-    COINS = 'coins'
 }
+
+interface Currency {
+    value: number;
+    type: CurrencyType;
+    quantity: number;
+}
+
+const currencies: Currency[] = [
+    {value: 500, type: CurrencyType.BILL, quantity: 2},
+    {value: 200, type: CurrencyType.BILL, quantity: 3},
+    {value: 100, type: CurrencyType.BILL, quantity: 5},
+    {value: 50, type: CurrencyType.BILL, quantity: 12},
+    {value: 20, type: CurrencyType.BILL, quantity: 20},
+    {value: 10, type: CurrencyType.BILL, quantity: 50},
+    {value: 5, type: CurrencyType.BILL, quantity: 100},
+    {value: 2, type: CurrencyType.COIN, quantity: 250},
+    {value: 1, type: CurrencyType.COIN, quantity: 500}
+];
 
 export class AtmMachine {
 
-    private typeToValue = {
-        [CurrencyType.BILLS]: [500, 200, 100, 50, 20, 10],
-        [CurrencyType.COINS]: [5, 2, 1]
-    };
-
-    private MIN_BILL_AMOUNT = 10
-    private MAX_COIN_AMOUNT = 5
-
     public withdraw(amount: number) {
-        const output = []
-        this.findAvailableBillAndCoin(amount, output)
-        console.log(output.join('\n'))
-    }
-
-    private findAvailableBillAndCoin(amount: number, output: string[]) {
-        if (!amount) {
-            return
+        const output: string[] = [];
+        for (const currency of currencies) {
+            if (amount <= 0) break;
+            const count = this.getCurrencyCount(amount, currency);
+            if (count > 0) {
+                output.push(this.generateOutput(count, currency));
+                amount = this.updateCurrency(amount, currency, count);
+            }
         }
 
-        const availableAmount = this.findNearestAvailableAmount(amount)
-        console.log('availableAmount: ' + availableAmount)
-        console.log('amount: ' + amount)
+        if (amount > 0) {
+            throw new Error('The ATM machine has not enough money, please go to the nearest atm machine.');
+        }
 
-        const countOfCurrency = Math.floor(amount / availableAmount)
-        const amountType = this.getAmountType(availableAmount, countOfCurrency)
-        output.push(`${countOfCurrency} ${amountType} of ${availableAmount}.`)
-        const reminder = amount % availableAmount
-        this.findAvailableBillAndCoin(reminder, output)
+        console.log(output.join('\n'));
+        console.log(`ATM state after the withdrawal:`, currencies);
     }
 
-    private findNearestAvailableAmount(amountToWithdraw: number): number {
-        if (amountToWithdraw <= this.MIN_BILL_AMOUNT) {
-            return this.typeToValue[CurrencyType.COINS].find(coinAmount => coinAmount <= amountToWithdraw);
-        }
-        return this.typeToValue[CurrencyType.BILLS].find(billAmount => billAmount <= amountToWithdraw);
+    private getCurrencyCount(amount: number, currency: Currency): number {
+        return Math.min(currency.quantity, Math.floor(amount / currency.value));
     }
 
-    private getAmountType(availableAmount: number, countOfCurrency: number): CurrencyType {
-        if (availableAmount <= this.MAX_COIN_AMOUNT) {
-            return countOfCurrency > 1 ? CurrencyType.COINS : CurrencyType.COIN;
-        }
-        return countOfCurrency > 1 ? CurrencyType.BILLS : CurrencyType.BILL;
+    private updateCurrency(amount: number, currency: Currency, count: number): number {
+        currency.quantity -= count;
+        return amount - count * currency.value;
+    }
 
+    private generateOutput(count: number, currency: Currency): string {
+        return `${count} ${count > 1 ? currency.type + 's' : currency.type} of ${currency.value}.`;
     }
 }
 
--- tests/src/atm-machine/atm-machine.spec.ts
+++ tests/src/atm-machine/atm-machine.spec.ts
@@ -2,20 +2,37 @@ import {beforeEach, describe, expect, it, vi} from "vitest";
 import {AtmMachine} from "../../../src/atm-machine/atm-machine";
 
 describe('atm machine should', () => {
+
     const expectedOutput1 =
         '1 bill of 500.\n' +
         '2 bills of 200.\n' +
         '1 bill of 50.\n' +
         '1 bill of 20.\n' +
         '1 bill of 10.\n' +
-        '1 coin of 5.\n' +
+        '1 bill of 5.\n' +
         '1 coin of 2.'
 
     const expectedOutput2 =
-        '2 bills of 200.\n' +
+        '1 bill of 200.\n' +
+        '2 bills of 100.\n' +
         '1 bill of 20.\n' +
         '1 bill of 10.\n' +
         '2 coins of 2.'
+
+    const expectedOutput3 =
+        '1 bill of 500.\n' +
+        '3 bills of 100.\n' +
+        '11 bills of 50.\n' +
+        '18 bills of 20.\n' +
+        '1 bill of 10.\n' +
+        '1 bill of 5.'
+
+
+    const expectedOutput4 =
+        '47 bills of 10.\n' +
+        '98 bills of 5.\n' +
+        '247 coins of 2.\n' +
+        '371 coins of 1.'
 
     let atmMachine: AtmMachine;
     beforeEach(() => {
@@ -23,10 +40,14 @@ describe('atm machine should', () => {
         vi.spyOn(console, 'log')
     });
 
-    it.each([[987, expectedOutput1], [434, expectedOutput2]])('withdraw correct amount in bills', (amount: number, expectedOutput) => {
+    it.each([[987, expectedOutput1], [434, expectedOutput2], [1725, expectedOutput3],[1825, expectedOutput4]], )('withdraw correct amount for %i', (amount: number, expectedOutput) => {
 
         atmMachine.withdraw(amount)
 
         expect(console.log).toHaveBeenCalledWith(expectedOutput)
+    })
+
+    it('throw an error when the atm machine has not enough money', () => {
+        expect(() => atmMachine.withdraw(10000)).toThrowError('The ATM machine has not enough money, please go to the nearest atm machine.')
     })
 })
