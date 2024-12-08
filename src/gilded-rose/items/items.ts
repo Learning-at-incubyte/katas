6ec46e699748e6c31be85171e891ec2ee1930c75
Vijay Makwana <vijay@incubyte.co>
2024-12-08 23:52:17 +0530

    add common functionality of item in Item class

--- src/gilded-rose/items/items.ts
+++ src/gilded-rose/items/items.ts
@@ -3,11 +3,28 @@ export class Item {
     sellIn: number;
     quality: number;
 
-    constructor(name:string, sellIn:number, quality:number) {
+    constructor(name: string, sellIn: number, quality: number) {
         this.name = name;
         this.sellIn = sellIn;
         this.quality = quality;
     }
 
-    updateQuality() {}
+    updateQuality() {
+        this.decreaseSellIn()
+        this.decreaseQuality()
+        if (this.sellIn < 0) {
+            this.decreaseQuality()
+        }
+    }
+
+    decreaseSellIn() {
+        this.sellIn--
+    }
+
+    decreaseQuality() {
+        if (this.quality > 0) {
+            this.quality--
+        }
+    }
+
 }
