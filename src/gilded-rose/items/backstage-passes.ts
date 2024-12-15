df963e16ebe47ed2dd70f29f0d4187740bcda324
Vijay Makwana <vijay@incubyte.co>
2024-12-15 17:37:54 +0530

    add constant for backstage item

--- src/gilded-rose/items/backstage-passes.ts
+++ src/gilded-rose/items/backstage-passes.ts
@@ -1,13 +1,20 @@
 import {Item} from "./items";
-import {MAX_QUALITY, MIN_QUALITY, MIN_SELL_IN, QUALITY_CHANGE_DOUBLE, QUALITY_CHANGE_TRIPLE} from "./constant";
+import {
+    MAX_QUALITY,
+    MIN_SELL_IN,
+    QUALITY_CHANGE_DOUBLE,
+    QUALITY_CHANGE_TRIPLE,
+    SELL_IN_THRESHOLD_DOUBLE_QUALITY,
+    SELL_IN_THRESHOLD_TRIPLE_QUALITY
+} from "./constant";
 
 export class BackstagePasses extends Item {
     updateQuality() {
         this.decreaseSellIn()
         if (this.quality < MAX_QUALITY) {
-            if (this.sellIn <= 10 && this.sellIn > 5) {
+            if (this.sellIn <= SELL_IN_THRESHOLD_DOUBLE_QUALITY && this.sellIn > SELL_IN_THRESHOLD_TRIPLE_QUALITY) {
                 this.increaseQualityBy(QUALITY_CHANGE_DOUBLE)
-            } else if (this.sellIn <= 5 && this.sellIn >= MIN_SELL_IN) {
+            } else if (this.sellIn <= SELL_IN_THRESHOLD_TRIPLE_QUALITY && this.sellIn >= MIN_SELL_IN) {
                 this.increaseQualityBy(QUALITY_CHANGE_TRIPLE)
             }
         }
