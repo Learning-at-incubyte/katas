657d7b8f6b56e4cab77aad5a43b0c0f8afbc97b7
Vijay Makwana <vijay@incubyte.co>
2024-12-15 16:55:51 +0530

    remove name property from test cases

--- tests/src/gilded-rose/items/conjured.spec.ts
+++ tests/src/gilded-rose/items/conjured.spec.ts
@@ -3,25 +3,25 @@ import {Conjured} from "../../../../src/gilded-rose/items/conjured";
 
 describe('conjured item should', () => {
     it('decrease sellIn by 1 before sell date', () => {
-        const item = new Conjured('conjured', 10, 20);
+        const item = new Conjured(10, 20);
         item.updateQuality();
         expect(item.sellIn).toBe(9);
     });
 
     it('decrease quality by 2 before sell data', () => {
-        const item = new Conjured('conjured', 10, 20);
+        const item = new Conjured(10, 20);
         item.updateQuality();
         expect(item.quality).toBe(18);
     })
 
     it('decrease quality by 4 after sell date', () => {
-        const item = new Conjured('conjured', 0, 20);
+        const item = new Conjured(0, 20);
         item.updateQuality();
         expect(item.quality).toBe(16);
     });
 
     it('not decrease quality below 0', () => {
-        const item = new Conjured('conjured', 10, 1);
+        const item = new Conjured( 10, 1);
         item.updateQuality();
         expect(item.sellIn).toBe(9);
         expect(item.quality).toBe(0);
