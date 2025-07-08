5cb29ced2d7ce14ad30800155e2a07fea8e676ab
Vijay Makwana <vijay@incubyte.co>
2025-07-08 10:33:22 +0530

    implement word frequency counting in text processing

--- src/text-processing/text-processing.ts
+++ src/text-processing/text-processing.ts
@@ -1,6 +1,15 @@
 export  class TextProcessing{
-
-    getMostCommonWords(_text: string, _number: number) {
-        throw new Error("Method not implemented.");
+    getMostCommonWords(text: string) {
+        const wordsCountMaps:Map<string, number> = new Map()
+        const words = text.toLowerCase().split(' ')
+        for (let word of words){
+            word = word.replace(/[^a-zA-Z0-9]/g,"")
+            if(wordsCountMaps.has(word)){
+                wordsCountMaps.set(word, wordsCountMaps.get(word)+1)
+            }else{
+                wordsCountMaps.set(word, 1)
+            }
+        }
+        return wordsCountMaps
     }
 }
--- tests/src/text-processing/text-processing.spec.ts
+++ tests/src/text-processing/text-processing.spec.ts
@@ -9,6 +9,36 @@ describe('text processing should', () => {
         textProcessing = new TextProcessing();
     })
 
+    it('count words frequency correctly by ignoring space and punctuation in text', () => {
+        const text = "Hello, this is an example for you to practice. You should grab this text and make it as your test case."
+        const expectedFrequency = new Map([
+            ['hello', 1],
+            ['this', 2],
+            ['is', 1],
+            ['an', 1],
+            ['example', 1],
+            ['for', 1],
+            ['you', 2],
+            ['to', 1],
+            ['practice', 1],
+            ['should', 1],
+            ['grab', 1],
+            ['text', 1],
+            ['and', 1],
+            ['make', 1],
+            ['it', 1],
+            ['as', 1],
+            ['your', 1],
+            ['test', 1],
+            ['case', 1]
+        ])
+
+        const result = textProcessing.getMostCommonWords(text);
+
+        expect(result).toEqual(expectedFrequency);
+    })
+
+
     it('return 10 most common words from text correctly', () => {
         const text = "Hello, this is an example for you to practice. You should grab this text and make it as your test case."
         const expectedWords = [
@@ -24,7 +54,7 @@ describe('text processing should', () => {
             'it'
         ]
 
-        const result = textProcessing.getMostCommonWords(text, 10);
+        const result = textProcessing.getMostCommonWords(text);
 
         expect(result).toEqual(expectedWords);
     })
