90baa8f735977169c84d6091fc9d37cb6cd1be5a
Vijay Makwana <vijay@incubyte.co>
2025-07-13 17:30:26 +0530

    add unit test to count total number of words in text

---
+++ tests/src/text-processor/text-processor.spec.ts
@@ -0,0 +1,19 @@
+import {beforeEach, describe, expect, it} from "vitest";
+import {TextProcessor} from "../../../src/text-processor/text-processor";
+
+
+describe('text processor', ()=>{
+    let textProcessor: TextProcessor;
+
+    beforeEach(()=>{
+        textProcessor = new TextProcessor();
+    })
+
+    it('count total number of words in text', ()=>{
+        const text = "hello good morning"
+
+        const result = textProcessor.processText(text)
+
+        expect(result).toBe(3)
+    })
+})
